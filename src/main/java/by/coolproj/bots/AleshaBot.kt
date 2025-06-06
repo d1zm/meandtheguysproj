4bc887f100db9bfb258156213f399d271968b8b6
Bogdan <avdeev1506@mail.ru>
2025-06-07 00:53:45 +0300

    Alesha bot init

--- src/main/java/by/coolproj/bots/AleshaBot.kt
+++ src/main/java/by/coolproj/bots/AleshaBot.kt
@@ -3,9 +3,10 @@ package by.coolproj.bots
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatAdministrators
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember
 
 
 @Component
@@ -20,17 +21,39 @@ class AleshaBot(
     override fun onUpdateReceived(update: Update) {
         if (update.hasMessage() && update.message.hasText()) {
             val messageText: String = update.message.text
-            val chatId: Long = update.message.chatId
 
-            val message = SendMessage()
-            message.chatId = chatId.toString()
-            message.text = "Вы написали: $messageText"
-
-            try {
-                execute(message)
-            } catch (e: TelegramApiException) {
-                e.printStackTrace()
+            if (messageText == "/all") {
+                try {
+                    val chatId = update.message.chatId
+                    val admins = getChatAdministrators(chatId) // Получаем список админов
+                    val mentions = admins.filterNot { it.user.isBot }.joinToString(", ") { admin ->
+                        val user = admin.user
+                        "<a href=\"tg://user?id=${user.id}\">${user.firstName}</a>"
+                    }
+                    sendTextMessage(chatId, mentions, parseMode = "HTML")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
+    }
+
+    // Функция для получения списка администраторов
+    private fun getChatAdministrators(chatId: Long): List<ChatMember> {
+        return try {
+            val response = execute(GetChatAdministrators().apply {
+                this.chatId = chatId.toString()
+            })
+            response
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+
+    // Упрощенная отправка сообщения
+    private fun sendTextMessage(chatId: Long, text: String, parseMode: String? = null) {
+        execute(SendMessage(chatId.toString(), text).apply {
+            this.parseMode = parseMode
+        })
     }
 }
